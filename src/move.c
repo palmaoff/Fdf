16b69d91a647efaffd763d3e2f04f4b0f7f90ad7
palmaoff <fedorin.anton@mail.ru>
2020-01-17 19:36:29 +0300

    kill move

--- src/move.c
+++ src/move.c
@@ -1,43 +1,75 @@
 #include "fdf.h"
 
-static	void move1(t_mlx *mlx, int x)
+void die(char *str)
 {
-	if (x == 5 || x == -5)
-		mlx->cam.x_r += x / 5 * 0.1;
-	else if (x == 6 || x == -6)
-		mlx->cam.y_r += x / 6 * 0.1;
-	else if (x == 7 || x == -7)
-		mlx->cam.z_r += x / 7 * 0.1;
-	else if (x == 8)
+	ft_putstr(str);
+	exit(0);
+}
+
+int key_press_too_too(int keycode, t_mlx *mlx)
+{
+	if (keycode == 82)
 	{
 		mlx->cam.x_r = 0;
 		mlx->cam.y_r = 0;
 		mlx->cam.z_r = 0;
 		mlx->cam.di = 1;
+		mlx->cam.z = 20;
 		mlx->cam.x = X_START_POS;
 		mlx->cam.y = Y_START_POS;
 	}
+	if (mlx->cam.z < 1)
+		mlx->cam.z = 1;
+	print_map(mlx);
+	return (0);
 }
 
-void	move_it(t_mlx *mlx, int x)
+int key_press_too(int keycode, t_mlx *mlx)
 {
+	if (keycode == 69) 
+	{
+		if (mlx->cam.z > 0)
+			mlx->cam.z += 1;
+	}
+	else if (keycode == 88) 
+		mlx->cam.x_r -= 0.1;
+	else if (keycode == 85) 
+		mlx->cam.x_r += 0.1;
+	else if (keycode == 83) 
+		mlx->cam.y_r += 0.1;
+	else if (keycode == 84) 
+		mlx->cam.y_r -= 0.1;
+	else if (keycode == 86) 
+		mlx->cam.z_r += 0.1;
+	else if (keycode == 87) 
+		mlx->cam.z_r -= 0.1;
+	key_press_too_too(keycode, mlx);
+	return (0);
+}
 
+int key_press(int keycode, t_mlx *mlx)
+{
 	mlx_destroy_image(mlx->mlx, (*mlx).img.img_ptr);
 	img_new(mlx);
-	if (x == 1 || x == -1)
-		mlx->cam.y += 5 * x;
-	else if (x == 2 || x == -2)
-		mlx->cam.x += 5 * x / 2;
-	else if (x == 3 || x == -3)
-		mlx->cam.di += 0.1 * x / 3;
-	else if (x == 4 || x == -4)
+	if (keycode == 53)
+		exit(0);
+	else if (keycode == 13 || keycode == 125)
+		mlx->cam.y += 5;
+	else if (keycode == 1 || keycode == 126)
+		mlx->cam.y -= 5;
+	else if (keycode == 0 || keycode == 124)
+		mlx->cam.x += 5;
+	else if (keycode == 2 || keycode == 123)
+		mlx->cam.x -= 5;
+	else if (keycode == 6) 
+		mlx->cam.di -= 0.1;
+	else if (keycode == 7) 
+		mlx->cam.di += 0.1;
+	else if (keycode == 78) 
 	{
-		if (x > 0 || mlx->cam.z > 0)
-			mlx->cam.z += x / 4;
-		if (mlx->cam.z < 1)
-			mlx->cam.z = 1;
+		if (mlx->cam.z > 0)
+			mlx->cam.z -= 1;
 	}
-	else if (x >= 5 || x <= -5)
-		move1(mlx, x);
-	print_map(mlx);
+	key_press_too(keycode, mlx);
+	return (0);
 }
